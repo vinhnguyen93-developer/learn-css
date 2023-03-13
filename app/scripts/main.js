b9b663471fb681abf714a402d186bbe707cdd2f9
Vinhnguyen93-developer <developerthienkim@gmail.com>
2023-03-13 13:59:47 +0700

    first commit

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,14 +1,21 @@
-window.addEventListener("load", function () {
-  const toggle = document.querySelector(".menu-toggle");
-  const menu = document.querySelector(".menu");
-  toggle && toggle.addEventListener("click", handleToggleMenu);
+window.addEventListener('load', function () {
+  const toggle = document.querySelector('.header-toggle');
+  const menu = document.querySelector('.header-menu');
+
+  toggle && toggle.addEventListener('click', handleToggleMenu);
+
   function handleToggleMenu(e) {
-    menu && menu.classList.add("is-show");
+    menu && menu.classList.add('is-expand');
   }
-  document.addEventListener("click", handleClickOutside);
+
+  document.addEventListener('click', handleClickOutside);
+
   function handleClickOutside(e) {
-    if (e.target.matches(".menu-toggle") || e.target.matches(".menu, .menu *"))
+    if (
+      e.target.matches('.header-toggle') ||
+      e.target.matches('.header-menu, .header-menu *')
+    )
       return;
-    menu && menu.classList.remove("is-show");
+    menu && menu.classList.remove('is-expand');
   }
 });
--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,16 +1,13 @@
-window.addEventListener("load", function () {
-  const toggle = document.querySelector(".menu-toggle");
-  const menu = document.querySelector(".menu");
-  toggle && toggle.addEventListener("click", handleToggleMenu);
-
+window.addEventListener('load', function () {
+  const toggle = document.querySelector('.header-toggle');
+  const menu = document.querySelector('.header-menu');
+  toggle && toggle.addEventListener('click', handleToggleMenu);
   function handleToggleMenu(e) {
-    menu && menu.classList.add("is-show");
+    menu && menu.classList.add('is-expand');
   }
-
-  document.addEventListener("click", handleClickOutside);
-
+  document.addEventListener('click', handleClickOutside);
   function handleClickOutside(e) {
-    if (e.target.matches(".menu-toggle") || e.target.matches(".menu, .menu *")) return;
-    menu && menu.classList.remove("is-show");
+    if (e.target.matches('.header-toggle') || e.target.matches('.header-menu, .header-menu *')) return;
+    menu && menu.classList.remove('is-expand');
   }
 });
