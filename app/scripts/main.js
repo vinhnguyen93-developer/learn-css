dc984414baaa586e09c468aad868cbebd1f09b40
*cECm6Vhjw5 <vinh@cck.co.jp>
2023-06-18 09:13:47 +0700

    [Implement] #5

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -27,5 +27,13 @@ $(document).ready(function () {
       "<button type='button' class='slick-prev pull-left'><i class='fal fa-angle-left' aria-hidden='true'></i></button>",
     nextArrow:
       "<button type='button' class='slick-next pull-right'><i class='fal fa-angle-right' aria-hidden='true'></i></button>",
+    responsive: [
+      {
+        breakpoint: 767,
+        settings: {
+          arrows: false,
+        },
+      },
+    ],
   });
 });
--- public/scripts/main.js
+++ public/scripts/main.js
@@ -16,6 +16,12 @@ window.addEventListener('load', function () {
 $(document).ready(function () {
   $('.quote-container').slick({
     prevArrow: "<button type='button' class='slick-prev pull-left'><i class='fal fa-angle-left' aria-hidden='true'></i></button>",
-    nextArrow: "<button type='button' class='slick-next pull-right'><i class='fal fa-angle-right' aria-hidden='true'></i></button>"
+    nextArrow: "<button type='button' class='slick-next pull-right'><i class='fal fa-angle-right' aria-hidden='true'></i></button>",
+    responsive: [{
+      breakpoint: 767,
+      settings: {
+        arrows: false
+      }
+    }]
   });
 });
